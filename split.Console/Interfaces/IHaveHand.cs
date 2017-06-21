367111fb1dc63eefc7a6373b701e7ebd7b29ada6
Aleks Tapinsh <aleks@tapinsh.com>
2017-06-20 21:32:13 -0400

    change decimal to int for amount, provide IHaveHand interface

---
+++ split.Console/Interfaces/IHaveHand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace split.Console.Interfaces
+{
+    public interface IHaveHand
+    {
+		/// <summary>
+		/// This method will contain Cards that Player has. 
+		/// </summary>
+		/// <returns>A list of objects that implement ICardInterface </returns>
+		List<ICardInterface> MyHand();
+    }
+}
