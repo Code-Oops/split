23faf9471f19e2675b3b63e0eefd0c5797afb60c
ryanbecker33 <rjb82@pitt.edu>
2017-06-26 19:30:14 -0400

    Created card struct and basic deck functionality
    Added a card struct as well as deck class. Able to create a deck, shuffle, and basic 1 card deal

---
+++ split.Console/Models/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace split.Console.Models
+{
+    public struct Card
+    {
+        private SuitType Suit;
+        private RankType Rank;
+
+        public Card(SuitType suit, RankType rank)
+        {
+            Suit = suit;
+            Rank = rank;
+        }
+
+        public SuitType GetSuit()
+        {
+            return Suit;
+        }
+
+        public RankType GetRank()
+		{
+			return Rank;
+		}
+    }
+}
