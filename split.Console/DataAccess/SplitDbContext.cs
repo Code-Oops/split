c02618d4e7c6c224a8517d7e2da32837b06b8092
Robert Taylor <rats3g@gmail.com>
2017-07-25 16:06:25 -0400

    Create database context and repository stubs for later use

---
+++ split.Console/DataAccess/SplitDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace split.Console.DataAccess
+{
+    public class SplitDbContext : DbContext
+    {
+        public SplitDbContext(DbContextOptions options) : base(options) {}
+    }
+}
---
+++ split.Console/DataAccess/SplitRepository.cs
@@ -0,0 +1,14 @@
+﻿using split.Console.Interfaces;
+
+namespace split.Console.DataAccess
+{
+    public class SplitRepository : ISplitRepository
+    {
+        private readonly SplitDbContext _context;
+
+        public SplitRepository(SplitDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
